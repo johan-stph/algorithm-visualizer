0f858cd9d7dc78942faf63065f83948625b58304
johan-stph <111685646+johan-stph@users.noreply.github.com>
2023-03-06 17:35:11 +0100

    create redux app

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,32 +1,58 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
+import logo from './logo.svg';
+import { Counter } from './features/counter/Counter';
 import './App.css';
 
-const  App = () => {
-  const [todos, setTodos] = useState<string[]>([]);
-  useEffect(
-    () => {
-      let todo = getTodos();
-      todo.then((value) => setTodos(value));
-    },
-    []
-  )
-  if (todos == null ||todos.length === 0) {
-    return <div>Loading...</div>;
-  }
-
+function App() {
   return (
-      <div>
-        {
-        todos.map((todo) => <div key={todo}>{todo}</div>)
-        }
-        </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <span>
+          <span>Learn </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux
+          </a>
+          <span>, </span>
+          <a
+            className="App-link"
+            href="https://redux-toolkit.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Redux Toolkit
+          </a>
+          ,<span> and </span>
+          <a
+            className="App-link"
+            href="https://react-redux.js.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React Redux
+          </a>
+        </span>
+      </header>
+    </div>
   );
-}
-
-
-async function getTodos(): Promise<string[]> {
-  const response = await fetch("http://localhost:8080/api/v1/todos");
-  return await response.json();
 }
 
 export default App;
