d9e490d78c9e4394e01e4ceae191f0fb3b7c216d
Johannes Stephan <111685646+johan-stph@users.noreply.github.com>
2023-03-08 18:23:09 +0100

    Added Thuenen Functionality and cleaned some code up (#6)
    * changed directory-strucutre
    
    * Added ThuenenModell

---
+++ engine/src/test/java/edu/kit/algorithms/cost_efficient_allocation/ThuenenModellTest.java
@@ -0,0 +1,65 @@
+package edu.kit.algorithms.cost_efficient_allocation;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+
+class ThuenenModellTest {
+
+    @Test
+    void solveThuenenModell() {
+        var inputList = List.of(new ThuenenModell.InputOfThuenenModell(
+               "Kartoffelanbau",
+                38,
+                7
+        ),
+                new ThuenenModell.InputOfThuenenModell(
+                        "Getreideanbau",
+                        20,
+                        4
+                ),
+                new ThuenenModell.InputOfThuenenModell(
+                        "Viehzucht",
+                        65,
+                        12
+                ),
+                new ThuenenModell.InputOfThuenenModell(
+                        "Industrie",
+                        110,
+                        40
+                ),
+                new ThuenenModell.InputOfThuenenModell(
+                        "Dienstleistungen",
+                        120,
+                        25
+                ));
+
+        List<ThuenenModell.ResultOfThuenenModell> resultOfThuenenModells = ThuenenModell.solveThuenenModell(inputList);
+        assertIterableEquals(
+                List.of(
+                        new ThuenenModell.ResultOfThuenenModell(
+                                "Dienstleistungen",
+                                0,
+                                4.230769230769231
+                        ),
+                        new ThuenenModell.ResultOfThuenenModell(
+                                "Viehzucht",
+                                4.230769230769231,
+                                5.4
+                        ),
+                        new ThuenenModell.ResultOfThuenenModell(
+                                "Kartoffelanbau",
+                                5.4,
+                                5.428571428571429
+                        )
+                ),
+                resultOfThuenenModells
+        );
+
+    }
+
+
+}
--- src/main/java/edu/kit/algorithms/Main.java
+++
@@ -1,6 +0,0 @@
-package edu.kit.algorithms;
-
-public class Main {
-    public static void main(String[] args) {
-    }
-}
