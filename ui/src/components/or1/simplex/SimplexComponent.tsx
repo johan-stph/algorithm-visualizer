112a4a1445d77f778c0b8189e6ee3ca698ddf2e5
johan-stph <111685646+johan-stph@users.noreply.github.com>
2023-03-15 01:49:52 +0100

    Fixed simplex component

--- ui/src/components/or1/simplex/SimplexComponent.tsx
+++ ui/src/components/or1/simplex/SimplexComponent.tsx
@@ -1,147 +1,212 @@
 import React, {useState} from 'react';
 
 export default function SimplexComponent() {
-    const [variableCount, setVariableCount] = useState(3);
-    const [constraintCount, setConstraintCount] = useState(2);
+    const [goalCoefficient, setGoalCoefficient] = useState<number[]>(Array(3).fill(0));
+    const [constraintCoefficients, setConstraintCoefficients] = useState<number[][]>(Array(3)
+        .fill(Array(4)
+            .fill(0)));
+    const [minOrMax, setMinOrMax] = useState<"min" | "max">("max");
     return (
         <div className="flex flex-col items-center min-h-screen bg-gray-100">
             <h1 className="text-3xl font-bold my-8">Simplex Algorithmus</h1>
             <div className="flex items-center justify-end">
-
-
-                <td className="py-2">
+                <div className="py-2">
                     <span className="mx-2">Variable Count</span>
-                </td>
-                <td className="py-2">
-                    <input className="w-full border border-gray-300 p-2 rounded-lg"
-                    type="number"
-                    min="1"
-                    max="5"
-                    defaultValue={variableCount}
-                    onChange={(e) => {
-                        if (e.target.value === "" || e.target.value === null || e.target.value === undefined) {
-                            return
-                        }
-                        let intValue = parseInt(e.target.value)
-                        if (!intValue || intValue > 5 || intValue < 1) {
-                            return
-                        }
-                        setVariableCount(parseInt(e.target.value))
-                    }}/>
-                </td>
+                </div>
+                <div className="py-2">
+                    <input
+                        className="w-full border border-gray-300 p-2 rounded-lg"
+                        type="number"
+                        min="1"
+                        max="5"
+                        defaultValue={constraintCoefficients[0].length - 1}
+                        onChange={(e) => {
+                            if (e.target.value === "" || e.target.value === null || e.target.value === undefined) {
+                                return
+                            }
+                            let intValue = parseInt(e.target.value)
+                            if (!intValue || intValue > 5 || intValue < 1) {
+                                return
+                            }
+                            setConstraintCoefficients(prev => {
+                                return prev.map(row => {
+                                    if (row.length < intValue) {
+                                        return [...row, 0]
+                                    } else {
+                                        return row.slice(0, intValue)
+                                    }
+                                })
+                            })
+                        }}
+                    />
+                </div>
 
-                <td className="py-2">
+                <div className="py-2">
                     <span className="mx-2">Constraint Count</span>
-                </td>
-                <td className="py-2">
-                    <input className="w-full border border-gray-300 p-2 rounded-lg"
-                    type="number"
-                    min="1"
-                    max="5"
-                    defaultValue={constraintCount}
-                    onChange={(e) => {
-                        if (e.target.value === "" || e.target.value === null || e.target.value === undefined) {
-                            return
-                        }
-                        let intValue = parseInt(e.target.value)
-                        if (!intValue || intValue > 5 || intValue < 1) {
-                            return
-                        }
-                        setConstraintCount(parseInt(e.target.value))
-                    }}/>
-                </td>
+                </div>
+                <div className="py-2">
+                    <input
+                        className="w-full border border-gray-300 p-2 rounded-lg"
+                        type="number"
+                        min="1"
+                        max="5"
+                        defaultValue={constraintCoefficients.length}
+                        onChange={(e) => {
+                            if (e.target.value === "" || e.target.value === null || e.target.value === undefined) {
+                                return
+                            }
+                            let intValue = parseInt(e.target.value)
+                            if (!intValue || intValue > 5 || intValue < 1) {
+                                return
+                            }
+                            setConstraintCoefficients(prev => {
+                                if (prev.length < intValue) {
+                                    return [...prev, Array(prev[0].length).fill(0)]
+                                } else {
+                                    return prev.slice(0, intValue)
+                                }
+                            })
+
+                        }}
+                    />
+                </div>
             </div>
 
-            <tr>
-                <td className="text-left">
-                    <div>
-                        <table className="w-full" cellSpacing="1" cellPadding="1">
-                            <tbody>
-                            <tr>
-                                <td>
-                                    <table>
-                                        <tbody>
-                                        {convertToGoalRow(variableCount)}
-                                        </tbody>
-                                    </table>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className={"text-xl"}>Subject to constraints</td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <table className="w-full" cellSpacing="1" cellPadding="1">
-                                        <tbody>
-                                        {[...Array(constraintCount)].map((__, _) =>
-                                            convertToTableRow(variableCount))
-                                        }
-                                        </tbody>
-                                    </table>
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
+            <div className="text-left">
+                <div className="w-full">
+                    <div className={"flex justify-center"}>
+                        {convertToGoalRow(constraintCoefficients[0].length - 1, goalCoefficient, setGoalCoefficient,
+                            minOrMax, setMinOrMax)}
                     </div>
-                </td>
-            </tr>
+                </div>
+
+                <div className="text-xl text-left">Subject to constraints</div>
+
+                <div className="w-full">
+                    {[...Array(constraintCoefficients.length)].map((__, i) => {
+                            return convertToTableRow(constraintCoefficients[0].length - 1, i, constraintCoefficients[i], setConstraintCoefficients)
+                        }
+                    )}
+                </div>
+            </div>
+        </div>
+
+
+    );
+}
+
+const convertToGoalRow = (amountOfVariables: number, goalCoefficients: number[],
+                          setGoalCoefficients: (prev: number[]) => void,
+                          minOrMax: string, setMinOrMax: (prev: any) => void) => {
+    return (
+        <div className="border-b border-gray-200 flex hover:bg-gray-100">
+            <div className="py-2 flex items-center m-2">
+                <select
+                    className="mr-2 border border-gray-300 p-2 rounded-lg"
+                    defaultValue="Max"
+                    onChange={(e) => setMinOrMax(e.target.value)}
+                >
+                    <option value="Max">Max</option>
+                    <option value="Min">Min</option>
+                </select>
+                <span className="mr-2">Z</span> =
+            </div>
+            <div className="py-2 flex items-center">
+                {[...Array(amountOfVariables)].map((_, i) => (
+                    <>
+                        <input
+                            className="w-full border border-gray-300 p-2 rounded-lg"
+                            value={goalCoefficients[i]}
+                            onChange={(e) => {
+                                if (e.target.value === "" || e.target.value === null || e.target.value === undefined) {
+                                    return;
+                                }
+                                let intValue = parseInt(e.target.value);
+                                if (!intValue) {
+                                    return;
+                                }
+                                let newGoalCoefficients = [...goalCoefficients];
+                                newGoalCoefficients[i] = intValue;
+                                setGoalCoefficients(newGoalCoefficients);
+                            }}
+                            type="number"
+                        />
+                        <span className="mx-2">x{i + 1}</span>
+                        {i < amountOfVariables - 1 ? "+" : ""}
+                    </>
+                ))}
+            </div>
         </div>
 
     );
 }
 
-const convertToGoalRow = (amountOfVariables: number) => {
+const convertToTableRow = (amountOfVariables: number, constraintIndex: number, constraintCoefficients: number[],
+                           setConstraintCoefficients: (prev: any) => void) => {
     return (
-        <tr className="border-b border-gray-200 hover:bg-gray-100">
-            <td className="py-2">
-                <select className="mr-2 border border-gray-300 p-2 rounded-lg">
-                    <option selected>Max</option>
-                    <option>Min</option>
-                </select>
-                <span className="mr-2">Z</span> =
-            </td>
+        <div className="border-b border-gray-200 hover:bg-gray-100 flex items-center">
             {[...Array(amountOfVariables)].map((_, i) =>
-                <>
-                    <td className="py-2">
-                        <input className="w-full border border-gray-300 p-2 rounded-lg"
-                               defaultValue={2} type="text"/>
-                    </td>
-                    <td className="py-2">
-                        <span className="mx-2">x{i + 1}</span> {i < amountOfVariables - 1 ? "+" : ""}
-                    </td>
-                </>
-            )
-            }
-        </tr>
-    );
+                <div key={`${amountOfVariables}, ${i}`} className="py-2 flex items-center">
+                    <input className="w-full border border-gray-300 p-2 rounded-lg"
+                           value={constraintCoefficients[i]}
+                           onChange={(e) => {
+                               if (e.target.value === "" || e.target.value === null || e.target.value === undefined) {
+                                   return
+                               }
+                               let intValue = parseInt(e.target.value)
+                               if (!intValue) {
+                                   return
+                               }
+                               let newConstraintCoefficients = [...constraintCoefficients]
+                               newConstraintCoefficients[i] = intValue
+
+                               setConstraintCoefficients((prev: number[][]) => {
+                                   let newPrev = [...prev]
+                                   newPrev[constraintIndex] = newConstraintCoefficients
+                                   return newPrev
+                               })
+
+                           }}
+                           type="number"/>
+                    <span className="mx-2">x{i + 1}</span>
+                    {i < amountOfVariables - 1 && <span>+</span>}
+                </div>
+            )}
+            <div className="py-2">
+                <select className="mr-2 border border-gray-300 p-2 rounded-lg">
+                    <option defaultValue="&lt;=">&lt;=</option>
+                    <option value="&gt;=">&gt;=</option>
+                    <option value="=">=</option>
+                </select>
+            </div>
+            <div className="py-2 flex-1">
+                <input className="w-full border border-gray-300 p-2 rounded-lg"
+                       value={constraintCoefficients[constraintCoefficients.length - 1]}
+                       onChange={(e) => {
+                           if (e.target.value === "" || e.target.value === null || e.target.value === undefined) {
+                               return
+                           }
+                           let intValue = parseInt(e.target.value)
+                           if (!intValue) {
+                               return
+                           }
+                           let newConstraintCoefficients = [...constraintCoefficients]
+                           newConstraintCoefficients[constraintCoefficients.length - 1] = intValue
+
+                           setConstraintCoefficients((prev: number[][]) => {
+                               let newPrev = [...prev]
+                               newPrev[constraintIndex] = newConstraintCoefficients
+                               return newPrev
+                           })
+
+                       }}
+                       type="number"/>
+            </div>
+        </div>
+
+    )
 }
 
-const convertToTableRow = (amountOfVariables: number) => {
-    return (
-        <tr className="border-b border-gray-200 hover:bg-gray-100">
-            {[...Array(amountOfVariables)].map((_, i) =>
-                <>
-                    <td className="py-2">
-                        <input className="w-full border border-gray-300 p-2 rounded-lg"
-                               defaultValue={2} type="text"/>
-                    </td>
-                    <td className="py-2">
-                        <span className="mx-2">x{i + 1}</span> {i < amountOfVariables - 1 ? "+" : ""}
-                    </td>
-                </>
-            )
-            }
-            <td className="py-2">
-                <select className="mr-2 border border-gray-300 p-2 rounded-lg">
-                    <option selected>&lt;=</option>
-                    <option>&gt;=</option>
-                    <option>=</option>
-                </select>
-            </td>
-            <td className="py-2">
-                <input className="w-full border border-gray-300 p-2 rounded-lg"
-                       defaultValue={50} type="text"/>
-            </td>
-        </tr>
-    )
+const solveTableau = (goalCoefficient: number[], constraintCoefficients: number[][], minOrMax: string) => {
+
 }
