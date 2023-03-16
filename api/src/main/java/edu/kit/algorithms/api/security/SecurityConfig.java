aca2e91bbff1d45299caa9cb74188e7c3f319402
johan-stph <111685646+johan-stph@users.noreply.github.com>
2023-03-16 15:25:22 +0100

    disabled CRSF

--- api/src/main/java/edu/kit/algorithms/api/security/SecurityConfig.java
+++ api/src/main/java/edu/kit/algorithms/api/security/SecurityConfig.java
@@ -18,6 +18,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         authorize -> authorize
                                 .anyRequest().permitAll()
