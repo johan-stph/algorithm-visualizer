ccf9dd015d1cbe3e7f78ae339f3db7ffe87c10da
johan-stph <111685646+johan-stph@users.noreply.github.com>
2023-03-16 16:08:28 +0100

    added tests and fixed a bug with simplex

--- engine/src/test/java/edu/kit/algorithms/or1/model/SimplexModelTest.java
+++ engine/src/test/java/edu/kit/algorithms/or1/model/SimplexModelTest.java
@@ -11,10 +11,69 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SimplexModelTest {
 
-    private SimplexModel simplexModel;
+    private SimplexModel simplexModelOne;
+    private SimplexModel simplexModelTwo;
+
+    private SimplexModel simplexModelThree;
+
 
     @BeforeEach
     void setUp() {
+        setUpOne();
+        setUpTwo();
+        setUpThree();
+    }
+
+    void setUpThree() {
+        String[] variables = {"x1", "x2"};
+        double[] goalCoefficient = {1, 1};
+        double[][] constraints = {
+                {1, 1, 1},
+                {1, 1, 0},
+        };
+        List<Tupel<EquationUtils, Double>> constraintsList = List.of(
+                new Tupel<>(EquationUtils.LESS_THAN_OR_EQUAL_TO, 1.0),
+                new Tupel<>(EquationUtils.LESS_THAN_OR_EQUAL_TO, 1.0)
+        );
+
+        simplexModelThree = new SimplexModel(
+                variables,
+                goalCoefficient,
+                MaxOrMin.MAX,
+                constraints,
+                constraintsList
+        );
+    }
+
+
+    void setUpTwo() {
+        String[] variables = {"x1", "x2", "x3"};
+        double[] goalCoefficient = {1, 1, 1};
+        double[][] constraints = {
+                {1, 1, 1},
+                {1, 1, 0},
+                {1, 0, 1},
+                {0, 1, 1}
+        };
+        List<Tupel<EquationUtils, Double>> constraintsList = List.of(
+                new Tupel<>(EquationUtils.LESS_THAN_OR_EQUAL_TO, 1.0),
+                new Tupel<>(EquationUtils.LESS_THAN_OR_EQUAL_TO, 1.0),
+                new Tupel<>(EquationUtils.LESS_THAN_OR_EQUAL_TO, 1.0),
+                new Tupel<>(EquationUtils.LESS_THAN_OR_EQUAL_TO, 1.0)
+        );
+
+        simplexModelTwo = new SimplexModel(
+                variables,
+                goalCoefficient,
+                MaxOrMin.MAX,
+                constraints,
+                constraintsList
+        );
+    }
+
+
+
+    void setUpOne() {
         String[] variables = {"x1", "x2"};
         double[] goalCoefficient = {30, 25};
         double[][] constraints = {
@@ -28,7 +87,7 @@ class SimplexModelTest {
                 new Tupel<>(EquationUtils.LESS_THAN_OR_EQUAL_TO, 9.0)
         );
 
-        simplexModel = new SimplexModel(
+        simplexModelOne = new SimplexModel(
                 variables,
                 goalCoefficient,
                 MaxOrMin.MAX,
@@ -40,7 +99,7 @@ class SimplexModelTest {
 
     @Test
     void isStandardForm() {
-        assertTrue(simplexModel.isStandardForm());
+        assertTrue(simplexModelOne.isStandardForm());
     }
 
     @Test
@@ -59,7 +118,48 @@ class SimplexModelTest {
                 null
         );
 
-        SimplexTableau simplexTableau = simplexModel.toTablou();
+        SimplexTableau simplexTableau = simplexModelOne.toTablou();
         assertEquals(simplex, simplexTableau);
     }
+
+    @Test
+    void toTablou2() {
+        SimplexTableau simplex = new SimplexTableau(
+                new double[][]{
+                        {1, 1, 1},
+                        {1, 1, 0},
+                        {1, 0, 1},
+                        {0, 1, 1}
+                },
+                new String[]{"x4", "x5", "x6", "x7"},
+                new String[]{"x1", "x2", "x3"},
+                new double[]{1.0, 1.0, 1.0, 1.0},
+                new double[]{-1.0, -1.0, -1.0},
+                0,
+                null
+        );
+
+        SimplexTableau simplexTableau = simplexModelTwo.toTablou();
+        assertEquals(simplex, simplexTableau);
+    }
+
+    @Test
+    void toTablou3() {
+        SimplexTableau simplex = new SimplexTableau(
+                new double[][]{
+                        {1, 1, 1},
+                        {1, 1, 0},
+                },
+                new String[]{"x3", "x4"},
+                new String[]{"x1", "x2"},
+                new double[]{1.0, 1.0},
+                new double[]{-1.0, -1.0},
+                0,
+                null
+        );
+
+        SimplexTableau simplexTableau = simplexModelThree.toTablou();
+        assertEquals(simplex, simplexTableau);
+    }
+
 }
